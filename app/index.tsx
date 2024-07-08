c7c45d0fb3b8361aa3b8e50610b709b0b7c84504
TECHGAMESPI <nailie2010@gmail.com>
2024-07-08 02:53:46 -0300

    Add button for exit the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { StyleSheet, BackHandler } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, BackHandler, View, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
 
@@ -7,6 +7,7 @@ const initialUrl = 'https://puzzlee.vercel.app/';
 
 export default function App() {
   const webViewRef = useRef(null);
+  const [isInitialPage, setIsInitialPage] = useState(true);
 
   const onBackPress = () => {
     if (webViewRef.current) {
@@ -23,6 +24,7 @@ export default function App() {
   }, []);
 
   const handleNavigationStateChange = (navState) => {
+    setIsInitialPage(navState.url === initialUrl);
     if (!navState.canGoBack && navState.url === initialUrl) {
       // Do nothing if we're at the initial URL
       return;
@@ -33,13 +35,24 @@ export default function App() {
     }
   };
 
+  const handleExitPress = () => {
+    BackHandler.exitApp();
+  };
+
   return (
-    <WebView
-      ref={webViewRef}
-      style={styles.container}
-      source={{ uri: initialUrl }}
-      onNavigationStateChange={handleNavigationStateChange}
-    />
+    <View style={styles.container}>
+      <WebView
+        ref={webViewRef}
+        style={styles.webview}
+        source={{ uri: initialUrl }}
+        onNavigationStateChange={handleNavigationStateChange}
+      />
+      {isInitialPage && (
+        <TouchableOpacity style={styles.button} onPress={handleExitPress}>
+          <Text style={styles.buttonText}>Sair</Text>
+        </TouchableOpacity>
+      )}
+    </View>
   );
 }
 
@@ -47,5 +60,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
+  },
+  webview: {
+    flex: 1,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    backgroundColor: '#ff4d4d', // Red background color
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3, // Add shadow for Android
+    shadowColor: '#000', // Add shadow for iOS
+    shadowOffset: { width: 0, height: 2 }, // Add shadow for iOS
+    shadowOpacity: 0.8, // Add shadow for iOS
+    shadowRadius: 2, // Add shadow for iOS
+  },
+  buttonText: {
+    color: '#fff', // White text color
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
